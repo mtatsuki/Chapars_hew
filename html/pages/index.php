8cc5835360c084eb995e6b600e8d5e79e950abd9
yosa34 <osaru.san.0234@gmail.com>
2018-11-27 17:08:42 +0900

    ファイル追加

---
+++ html/pages/index.php
@@ -0,0 +1,12 @@
+
+<?php
+
+//==================================
+
+include "./tpl/heder.php";
+include "./tpl/index.php";
+include "./tpl/fooder.html";
+
+//==================================
+
+?>
