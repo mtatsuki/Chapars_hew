756caf29df3f8f9a2f791ba77859fa2daa2f3996
yosa34 <osaru.san.0234@gmail.com>
2018-11-29 00:11:08 +0900

    style修正

--- html/pages/tpl/header.php
+++ html/pages/tpl/header.php
@@ -27,8 +27,8 @@
   <script type="text/javascript"  src="../js/javascript_style.js"></script>
   <!-- jqueryバージョン３ -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
   <title>jquery</title>
 </head>
 <body>
+
   
