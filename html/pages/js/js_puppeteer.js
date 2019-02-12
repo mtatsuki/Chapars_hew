40eb1b195a004698ef0f1ad117088b4ed3855eb7
yosa34 <osaru.san.0234@gmail.com>
2019-02-12 10:07:53 +0900

    aaa

--- html/pages/js/js_puppeteer.js
+++ html/pages/js/js_puppeteer.js
@@ -1,14 +1,17 @@
-'use strict';
+'use strict';//エラー強化的な？
 
 const puppeteer = require('puppeteer');
 
-async function downloads() {
-    //仮装ブラウザ
+
+//ログイン関数
+async function downloads(){
+
     const browser = await puppeteer.launch({headless:false});
     //新タブ
     const page = await browser.newPage();
     //移動
-    await page.goto("http://localhost:8888");
+    await page.goto("https://blog.tes.co.jp/entry/2018/03/14/193601");
 
 }//downloads
+
 downloads();
