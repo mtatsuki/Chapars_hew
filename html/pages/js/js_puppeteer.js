75c34fc10169260e4d6690813583361484e7d430
yosa34 <osaru.san.0234@gmail.com>
2019-02-12 15:32:36 +0900

    ログイン画面までgoto

--- html/pages/js/js_puppeteer.js
+++ html/pages/js/js_puppeteer.js
@@ -1,5 +1,12 @@
 'use strict';//エラー強化的な？
 
+// const numValue = 100;                  //数字
+// const strValue = "私は値です！";         //文字
+// const boolValue = true;                //true/false
+// const arrayValue = [1, 2, 3];          //配列
+// const objValue = { key: 'value' };     //連想配列
+
+
 const puppeteer = require('puppeteer');
 
 
@@ -9,8 +16,10 @@ async function downloads(){
     const browser = await puppeteer.launch({headless:false});
     //新タブ
     const page = await browser.newPage();
+    // ページの大きさ定義
+    await page.setViewport({ width: 1200, height: 600 });
     //移動
-    await page.goto("https://blog.tes.co.jp/entry/2018/03/14/193601");
+    await page.goto("https://accounts.smaregi.jp/login?client_id=pos&_ga=2.40594788.79894584.1549952904-166718165.1549952904");
 
 }//downloads
 
