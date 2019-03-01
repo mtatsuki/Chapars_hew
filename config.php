5aff8978db5890bfb40241e3df5ee7241c24b940
Hydrochloric05 <meteo1005@gmail.com>
2019-03-01 11:35:29 +0900

    ファイル名変更

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-  define('DB','hew2019');
-  define('DB_HOST','localhost');
-  define('DB_USER','root');
-  define('DB_PASS','');
-  define('MeCab_URL','"C:\Program Files\MeCab\bin\mecab.exe"');
-?>
